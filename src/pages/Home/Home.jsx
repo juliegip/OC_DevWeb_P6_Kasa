f697f84d485d2fbce8402ec50fcaa3c0161dfc9f
juliegip <julie.gip@gmail.com>
2023-08-21 15:30:36 +0200

    initialisation du projet

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import data from '../../logements.json'
+
+
+function Home() {
+  console.log(data)
+  return (
+    <div>
+HEllo
+
+
+    </div>
+  );
+}
+
+export default Home;
