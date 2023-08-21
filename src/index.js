fad6a8ab4f9a17911888396a557f07f7730d3e3d
juliegip <julie.gip@gmail.com>
2023-08-21 16:33:37 +0200

    react-router installé et configuré

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Home from './pages/Home/Home';
+import { BrowserRouter, Route, Routes, Link, Switch } from 'react-router-dom';
+import Home from './pages/Home/P_Home';
+import About from './pages/About/P_About';
+import Header from './components/Header/Header';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
+    <Header />
     <Home />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+
+    </BrowserRouter>
   </React.StrictMode>
 );
 
